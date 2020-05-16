3214c660898841388c654b87f2f16474b826e175
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-16 18:29:48 +0530

    Added text field validation

--- app/src/main/java/com/dscvit/handly/ui/auth/SigninFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/auth/SigninFragment.kt
@@ -1,12 +1,15 @@
 package com.dscvit.handly.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.dscvit.handly.R
+import com.dscvit.handly.util.isValidEmail
 import kotlinx.android.synthetic.main.fragment_signin.*
 
 
@@ -29,7 +32,36 @@ class SigninFragment : Fragment() {
         }
 
         signin_button.setOnClickListener {
+            val email = signin_email_text.editText!!.text.toString()
+            val password = signin_password_text.editText!!.text.toString()
 
+            if (!email.isValidEmail()) {
+                signin_email_text.error = "Please enter a valid email"
+            }
+
+            if (password.length < 6) {
+                signin_password_text.error = "Password must have at least 6 characters"
+            }
+
+            signin_email_text.editText!!.doOnTextChanged {text, _, _, _ ->
+                if (text.isValidEmail()) {
+                    signin_email_text.error = null
+                } else {
+                    signin_email_text.error = "Please enter a valid email"
+                }
+            }
+
+            signin_password_text.editText!!.doOnTextChanged {text, _, _, _ ->
+                if (text!!.length > 5) {
+                    signin_password_text.error = null
+                } else {
+                    signin_password_text.error = "Password must have at least 6 characters"
+                }
+            }
+
+            if (email.isValidEmail() && password.length > 5) {
+                Log.d("esh", "Valid Login")
+            }
         }
     }
 }
--- app/src/main/java/com/dscvit/handly/ui/auth/SignupFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/auth/SignupFragment.kt
@@ -1,12 +1,16 @@
 package com.dscvit.handly.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.dscvit.handly.R
+import com.dscvit.handly.util.isValidEmail
+import kotlinx.android.synthetic.main.fragment_signin.*
 import kotlinx.android.synthetic.main.fragment_signup.*
 
 
@@ -27,6 +31,52 @@ class SignupFragment : Fragment() {
 
         signin_instead_text.setOnClickListener {
             findNavController().navigate(action)
+        }
+
+        signup_button.setOnClickListener {
+            val email = signup_email_text.editText!!.text.toString()
+            val password = signup_password_text.editText!!.text.toString()
+            val name = signup_name_text.editText!!.text.toString()
+
+            if (!email.isValidEmail()) {
+                signup_email_text.error = "Please enter a valid email"
+            }
+
+            if (password.trim().length < 6) {
+                signup_password_text.error = "Password must have at least 6 characters"
+            }
+
+            if (name.trim().length < 2) {
+                signup_name_text.error = "Please enter a valid name"
+            }
+
+            signup_email_text.editText!!.doOnTextChanged {text, _, _, _ ->
+                if (text.isValidEmail()) {
+                    signup_email_text.error = null
+                } else {
+                    signup_email_text.error = "Please enter a valid email"
+                }
+            }
+
+            signup_password_text.editText!!.doOnTextChanged {text, _, _, _ ->
+                if (text!!.trim().length > 5) {
+                    signup_password_text.error = null
+                } else {
+                    signup_password_text.error = "Password must have at least 6 characters"
+                }
+            }
+
+            signup_name_text.editText!!.doOnTextChanged {text, _, _, _ ->
+                if (text!!.trim().length >= 2) {
+                    signup_name_text.error = null
+                } else {
+                    signup_name_text.error = "Password must have at least 6 characters"
+                }
+            }
+
+            if (email.isValidEmail() && password.trim().length > 5 && name.trim().length >= 2) {
+                Log.d("esh", "Valid Sign Up")
+            }
         }
     }
 }
