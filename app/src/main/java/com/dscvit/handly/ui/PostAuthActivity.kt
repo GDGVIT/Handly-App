a2f71871164eb5ad7208a69d6e7ae013f36a2f60
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-16 11:59:52 +0530

    Added post and pre auth empty activity

--- app/src/main/java/com/dscvit/handly/ui/PostAuthActivity.kt
+++ app/src/main/java/com/dscvit/handly/ui/PostAuthActivity.kt
@@ -1,12 +1,13 @@
-package com.dscvit.handly
+package com.dscvit.handly.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dscvit.handly.R
 
-class MainActivity : AppCompatActivity() {
+class PostAuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_post_auth)
     }
 }
