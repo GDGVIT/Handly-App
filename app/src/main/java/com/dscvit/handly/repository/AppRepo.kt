a997f5537ef67312a644aca42edc807fb5e8aa50
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-19 16:16:01 +0530

    API: Added funs to repo

--- app/src/main/java/com/dscvit/handly/repository/AppRepo.kt
+++ app/src/main/java/com/dscvit/handly/repository/AppRepo.kt
@@ -1,7 +1,17 @@
 package com.dscvit.handly.repository
 
+import com.dscvit.handly.model.auth.SigninRequest
+import com.dscvit.handly.model.auth.SignupRequest
 import com.dscvit.handly.network.ApiClient
 
 class AppRepo(private val apiClient: ApiClient): BaseRepo() {
+
+    fun signupUser(signupRequest: SignupRequest) = makeRequest {
+        apiClient.signupUser(signupRequest)
+    }
+
+    fun signinUser(signinRequest: SigninRequest) = makeRequest {
+        apiClient.signinUser(signinRequest)
+    }
 
 }
