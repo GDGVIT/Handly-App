f2666cb0ab8b488f345da0e37fc72f23c62c3704
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-20 16:45:47 +0530

    UI: Made minor changes

--- app/src/main/java/com/dscvit/handly/ui/PostAuthActivity.kt
+++ app/src/main/java/com/dscvit/handly/ui/PostAuthActivity.kt
@@ -1,13 +1,14 @@
 package com.dscvit.handly.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.dscvit.handly.R
 import com.dscvit.handly.util.Constants
 import com.dscvit.handly.util.PrefHelper
+import com.google.android.material.tabs.TabLayout
 import com.onesignal.OneSignal
 import kotlinx.android.synthetic.main.activity_post_auth.*
 
@@ -20,5 +21,37 @@ class PostAuthActivity : AppCompatActivity() {
         val sharedPref = PrefHelper.customPrefs(this, Constants.PREF_NAME)
         Log.d("esh", sharedPref.getString(Constants.PREF_AUTH_TOKEN, "")!!)
         Log.d("esh", OneSignal.getPermissionSubscriptionState().subscriptionStatus.userId)
+
+        val navController = Navigation.findNavController(this, R.id.post_auth_nav_host)
+        val navOptions = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setEnterAnim(R.anim.fade_in)
+            .setExitAnim(R.anim.fade_out)
+            .setPopEnterAnim(R.anim.fade_in)
+            .setPopExitAnim(R.anim.fade_out)
+            .setPopUpTo(navController.graph.startDestination, false)
+            .build()
+
+        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab!!.position) {
+                    0 -> {
+                        navController.navigate(R.id.collectionsFragment, null, navOptions)
+                    }
+                    1 -> {
+                        navController.navigate(R.id.settingsFragment, null, navOptions)
+                    }
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        tab_layout.getTabAt(0)!!.select()
     }
 }
--- app/src/main/java/com/dscvit/handly/ui/home/SettingsFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/home/SettingsFragment.kt
@@ -1,10 +1,11 @@
-package com.dscvit.handly
+package com.dscvit.handly.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dscvit.handly.R
 
 
 class SettingsFragment : Fragment() {
