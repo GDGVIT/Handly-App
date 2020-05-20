a497cdc2995962338030c6497dc387b701f6fca4
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-20 18:42:03 +0530

    UI: Added signout button

--- app/src/main/java/com/dscvit/handly/ui/home/CollectionsFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/home/CollectionsFragment.kt
@@ -1,17 +1,13 @@
 package com.dscvit.handly.ui.home
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
-
 import com.dscvit.handly.R
 import com.dscvit.handly.adapter.CollectionsAdapter
 import com.dscvit.handly.model.Result
--- app/src/main/java/com/dscvit/handly/ui/home/SettingsFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/home/SettingsFragment.kt
@@ -1,11 +1,17 @@
 package com.dscvit.handly.ui.home
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.dscvit.handly.R
+import com.dscvit.handly.ui.PreAuthActivity
+import com.dscvit.handly.util.Constants
+import com.dscvit.handly.util.PrefHelper
+import com.dscvit.handly.util.PrefHelper.set
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 
 class SettingsFragment : Fragment() {
@@ -16,6 +22,21 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = PrefHelper.customPrefs(requireContext(), Constants.PREF_NAME)
+
+        signout_button.setOnClickListener {
+            sharedPref[Constants.PREF_AUTH_TOKEN] = ""
+            sharedPref[Constants.PREF_IS_AUTH] = false
+
+            val intent = Intent(requireContext(), PreAuthActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
     }
 
 }
