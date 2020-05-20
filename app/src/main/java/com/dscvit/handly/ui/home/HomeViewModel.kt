ac67dcf8fcd46f74f49924e08ad215e712ae1615
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-20 13:11:37 +0530

    VM: Update the viewmodel

--- app/src/main/java/com/dscvit/handly/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dscvit/handly/ui/home/HomeViewModel.kt
@@ -1,8 +1,18 @@
 package com.dscvit.handly.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.dscvit.handly.model.collection.CreateCollectionRequest
+import com.dscvit.handly.model.collection.UpdateCollection
 import com.dscvit.handly.repository.AppRepo
 
-class HomeViewModel(private val repo: AppRepo): ViewModel() {
+class HomeViewModel(private val repo: AppRepo) : ViewModel() {
+
+    fun createCollection(createCollectionRequest: CreateCollectionRequest) =
+        repo.createCollection(createCollectionRequest)
+
+    fun getCollections() = repo.getCollections()
+
+    fun updateCollections(updateCollection: UpdateCollection) =
+        repo.updateCollection(updateCollection)
 
 }
