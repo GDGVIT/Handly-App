209cd9341d1b5b541fb07d902bd363fdb22e83ce
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-20 18:03:05 +0530

    UI: Finished creation of folders

--- app/src/main/java/com/dscvit/handly/ui/home/CollectionsFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/home/CollectionsFragment.kt
@@ -1,20 +1,28 @@
 package com.dscvit.handly.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 
 import com.dscvit.handly.R
 import com.dscvit.handly.adapter.CollectionsAdapter
 import com.dscvit.handly.model.Result
+import com.dscvit.handly.model.collection.CreateCollectionRequest
 import com.dscvit.handly.util.hide
+import com.dscvit.handly.util.shortToast
 import com.dscvit.handly.util.show
+import com.github.ybq.android.spinkit.style.Circle
 import com.github.ybq.android.spinkit.style.Wave
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.add_collection_alert.view.*
 import kotlinx.android.synthetic.main.fragment_collections.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
@@ -45,6 +53,58 @@ class CollectionsFragment : Fragment() {
         }
 
         getCollection(homeViewModel, collectionAdapter)
+
+        collection_fab.setOnClickListener {
+            val dialogBuilder = MaterialAlertDialogBuilder(requireContext()).create()
+            val inflater = this.layoutInflater
+            val dialogView = inflater.inflate(R.layout.add_collection_alert, null)
+            dialogView.create_progress.setIndeterminateDrawable(Circle())
+            dialogView.create_progress.hide()
+
+            dialogView.create_button.setOnClickListener {
+                if (dialogView.create_name.text.isNotBlank()) {
+                    val createCollectionRequest = CreateCollectionRequest(
+                        dialogView.create_name.text.toString().trim()
+                    )
+                    homeViewModel.createCollection(createCollectionRequest)
+                        .observe(viewLifecycleOwner, Observer {
+                            when (it.status) {
+                                Result.Status.LOADING -> {
+                                    dialogView.create_title.hide()
+                                    dialogView.create_name.hide()
+                                    dialogView.create_button.hide()
+                                    dialogView.create_cancel.hide()
+                                    dialogView.create_progress.show()
+                                }
+                                Result.Status.SUCCESS -> {
+                                    getCollection(homeViewModel, collectionAdapter)
+                                    dialogBuilder.dismiss()
+                                }
+                                Result.Status.ERROR -> {
+                                    Log.d("esh", it.message!!)
+                                    shortToast("Oops something went wrong")
+
+                                    dialogView.create_title.show()
+                                    dialogView.create_name.show()
+                                    dialogView.create_button.show()
+                                    dialogView.create_cancel.show()
+                                    dialogView.create_progress.hide()
+                                }
+                            }
+                        })
+                } else {
+                    shortToast("Name cant be empty")
+                }
+            }
+
+            dialogView.create_cancel.setOnClickListener {
+                dialogBuilder.dismiss()
+            }
+
+            dialogBuilder.setView(dialogView)
+            dialogBuilder.setCancelable(false)
+            dialogBuilder.show()
+        }
     }
 
     private fun getCollection(
