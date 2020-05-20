e4dbb6e0244114bcdeb83b7747089939eb31549d
Rithik Jain <rithik.jain3006@gmail.com>
2020-05-20 19:30:52 +0530

    UI: Added alert box to modify collections

--- app/src/main/java/com/dscvit/handly/ui/home/CollectionsFragment.kt
+++ app/src/main/java/com/dscvit/handly/ui/home/CollectionsFragment.kt
@@ -12,14 +12,14 @@ import com.dscvit.handly.R
 import com.dscvit.handly.adapter.CollectionsAdapter
 import com.dscvit.handly.model.Result
 import com.dscvit.handly.model.collection.CreateCollectionRequest
-import com.dscvit.handly.util.hide
-import com.dscvit.handly.util.shortToast
-import com.dscvit.handly.util.show
+import com.dscvit.handly.model.collection.UpdateCollection
+import com.dscvit.handly.util.*
 import com.github.ybq.android.spinkit.style.Circle
 import com.github.ybq.android.spinkit.style.Wave
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.add_collection_alert.view.*
 import kotlinx.android.synthetic.main.fragment_collections.*
+import kotlinx.android.synthetic.main.modify_collection_alert.view.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 
@@ -49,6 +49,64 @@ class CollectionsFragment : Fragment() {
         }
 
         getCollection(homeViewModel, collectionAdapter)
+
+        collection_recycler_view.addOnItemLongClickListener(object : OnItemClickListener {
+            override fun onItemClicked(position: Int, view: View) {
+                val dialogBuilder = MaterialAlertDialogBuilder(requireContext()).create()
+                val inflater = this@CollectionsFragment.layoutInflater
+                val dialogView = inflater.inflate(R.layout.modify_collection_alert, null)
+                dialogView.modify_progress.setIndeterminateDrawable(Circle())
+                dialogView.modify_progress.hide()
+                dialogView.modify_name.setText(collectionAdapter.collectionList[position].name)
+
+                dialogView.modify_button.setOnClickListener {
+                    if (dialogView.modify_name.text.isNotBlank()) {
+                        val updateCollection = UpdateCollection(
+                            collectionAdapter.collectionList[position].id,
+                            dialogView.modify_name.text.toString().trim()
+                        )
+                        homeViewModel.updateCollections(updateCollection)
+                            .observe(viewLifecycleOwner, Observer {
+                                when (it.status) {
+                                    Result.Status.LOADING -> {
+                                        dialogView.modify_name.hide()
+                                        dialogView.modify_cancel.hide()
+                                        dialogView.modify_button.hide()
+                                        dialogView.modify_delete.hide()
+                                        dialogView.modify_title.hide()
+                                        dialogView.modify_progress.show()
+                                    }
+                                    Result.Status.SUCCESS -> {
+                                        getCollection(homeViewModel, collectionAdapter)
+                                        dialogBuilder.dismiss()
+                                    }
+                                    Result.Status.ERROR -> {
+                                        Log.d("esh", it.message!!)
+                                        shortToast("Oops something went wrong")
+
+                                        dialogView.modify_name.show()
+                                        dialogView.modify_cancel.show()
+                                        dialogView.modify_button.show()
+                                        dialogView.modify_delete.show()
+                                        dialogView.modify_title.show()
+                                        dialogView.modify_progress.hide()
+                                    }
+                                }
+                            })
+                    } else {
+                        shortToast("Name can't be empty")
+                    }
+                }
+
+                dialogView.modify_cancel.setOnClickListener {
+                    dialogBuilder.dismiss()
+                }
+
+                dialogBuilder.setView(dialogView)
+                dialogBuilder.setCancelable(false)
+                dialogBuilder.show()
+            }
+        })
 
         collection_fab.setOnClickListener {
             val dialogBuilder = MaterialAlertDialogBuilder(requireContext()).create()
@@ -89,7 +147,7 @@ class CollectionsFragment : Fragment() {
                             }
                         })
                 } else {
-                    shortToast("Name cant be empty")
+                    shortToast("Name can't be empty")
                 }
             }
 
