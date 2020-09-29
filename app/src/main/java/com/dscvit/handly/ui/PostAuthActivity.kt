ae4f0c88521ee437c0448fa6ff223823df7042b7
Rithik Jain <rithik.jain3006@gmail.com>
2020-09-29 19:06:49 +0530

    UI: Removed tab bar layout from post auth

--- app/src/main/java/com/dscvit/handly/ui/PostAuthActivity.kt
+++ app/src/main/java/com/dscvit/handly/ui/PostAuthActivity.kt
@@ -23,37 +23,5 @@ class PostAuthActivity : AppCompatActivity() {
         val sharedPref = PrefHelper.customPrefs(this, Constants.PREF_NAME)
         Log.d(TAG, sharedPref.getString(Constants.PREF_AUTH_TOKEN, "")!!)
         Log.d(TAG, OneSignal.getPermissionSubscriptionState().subscriptionStatus.userId)
-
-        val navController = Navigation.findNavController(this, R.id.post_auth_nav_host)
-        val navOptions = NavOptions.Builder()
-            .setLaunchSingleTop(true)
-            .setEnterAnim(R.anim.fade_in)
-            .setExitAnim(R.anim.fade_out)
-            .setPopEnterAnim(R.anim.fade_in)
-            .setPopExitAnim(R.anim.fade_out)
-            .setPopUpTo(navController.graph.startDestination, false)
-            .build()
-
-        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab!!.position) {
-                    0 -> {
-                        navController.navigate(R.id.collectionsFragment, null, navOptions)
-                    }
-                    1 -> {
-                        navController.navigate(R.id.settingsFragment, null, navOptions)
-                    }
-                }
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-        })
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        tab_layout.getTabAt(0)!!.select()
     }
 }
